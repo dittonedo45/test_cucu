50744239ff20c012bc924fb486e665f327b4a873
Eddington Chamunorwa <dittonedo45@gmail.com>
2023-05-06 16:15:28 +0200

    Test.

--- main.py
+++ main.py
@@ -24,5 +24,21 @@ def ugh():
         for j in map(lambda x: os.path.join(i, x),
                 f):
             yield j
+"""
 result = subprocess.run(['pip', 'install',  "tornado"],
         stdout=sys.stdout.buffer)
+"""
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write(Hello, world)
+
+def make_app():
+    return tornado.web.Application([
+        (r/, MainHandler),
+    ])
+
+if __name__ == __main__:
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
