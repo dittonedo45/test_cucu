047b5756e4eec54fd97923415707012f728b9ed4
Eddington Chamunorwa <dittonedo45@gmail.com>
2023-05-06 15:43:39 +0200

    Test if an error can be easily loggedw

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import setuptools
 
 def main():
-    o.setuptools.setup(
+    setuptools.setup(
             name="uggh",
             packages=setuptools.find_packages(),
             install_requires=[
