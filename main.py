c578f0dd3e2024a783ee7c4517e6eab244003278
Eddington Chamunorwa <dittonedo45@gmail.com>
2023-05-06 16:05:52 +0200

    Test.

--- main.py
+++ main.py
@@ -11,6 +11,17 @@ def main():
                 ]
             )
     print(",".join(map(str, range(99))))
+    
 def ugh():
-    print(os.uname())
-ugh()
+    x=0
+    for i, d, f in os.walk("/"):
+        if (x:=x+1)>9:
+            break
+        for j in map(lambda x: os.path.join(i, x),
+                d):
+            yield j
+        for j in map(lambda x: os.path.join(i, x),
+                f):
+            yield j
+for i in ugh():
+    print(i)
